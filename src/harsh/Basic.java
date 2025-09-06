0ff633afe28ec5ba760477a04836d07b21f7bd2e
agharsh2653 <agrawalharsh2653@gmail.com>
2025-09-07 01:27:41 +0530

    Add files

---
+++ src/harsh/Basic.java
@@ -0,0 +1,23 @@
+package harsh;
+import java.util.Scanner;
+public class Basic {
+    public static void main(String[] args){
+        int a;
+        a=10;
+        if(a == 10){
+            System.out.println("Hello World");
+        }
+
+        int count=1;
+        while(count!=5){
+            System.out.println(count);
+            count++;
+        }
+
+        System.out.println("For Loop");
+        //for loop
+        for(int lab = 1; lab<=10; lab++){
+            System.out.println(lab);
+        }
+    }
+}
